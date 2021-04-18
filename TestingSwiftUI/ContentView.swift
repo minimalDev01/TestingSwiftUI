39413667ea900214a1845472fc972f78f3b589e9
minimalDev01 <sergiocn.dev@gmail.com>
2021-04-18 20:40:57 +0200

    Initial Commit

---
+++ TestingSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestingSwiftUI
+//
+//  Created by Sergio Carralero Nuño on 18/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
